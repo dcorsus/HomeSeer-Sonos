354b5ccc9acd55f1855bc00565cb6104ecbac60b
dcorsus <dirk@famcorsus.com>
2019-12-14 12:07:23 -0800

    Changes to v51

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.1.0.50")>
-<Assembly: AssemblyFileVersion("3.1.0.50")>
+<Assembly: AssemblyVersion("3.1.0.51")>
+<Assembly: AssemblyFileVersion("3.1.0.51")>
